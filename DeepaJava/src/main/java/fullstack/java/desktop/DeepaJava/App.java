b2f346553370187eaf747e313d6d4b4870b490dc
razzaksr <razzaksr@gmail.com>
2021-07-11 13:18:21 +0530

    First commit

---
+++ DeepaJava/src/main/java/fullstack/java/desktop/DeepaJava/App.java
@@ -0,0 +1,13 @@
+package fullstack.java.desktop.DeepaJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Zealous Tech Corp" );
+    }
+}
