7c224d94ad1af0f2cbce91def4a2d8eb0c072e1c
razzaksr <razzaksr@gmail.com>
2021-08-28 11:31:22 +0530

    revoke

--- DeepaJavaBasics/src/main/java/basics/deepa/java/DeepaJavaBasics/files/AppendByStreams.java
+++ DeepaJavaBasics/src/main/java/basics/deepa/java/DeepaJavaBasics/files/AppendByStreams.java
@@ -6,7 +6,6 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-
 public class AppendByStreams 
 {
 	public static void main(String[] args) throws IOException 
