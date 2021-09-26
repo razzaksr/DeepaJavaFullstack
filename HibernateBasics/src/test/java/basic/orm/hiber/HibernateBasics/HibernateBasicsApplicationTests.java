f48c09d053e9cc96c9d4d6998743337b1fa2aaf6
razzaksr <razzaksr@gmail.com>
2021-09-26 12:54:52 +0530

    Hibernate started

---
+++ HibernateBasics/src/test/java/basic/orm/hiber/HibernateBasics/HibernateBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package basic.orm.hiber.HibernateBasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
