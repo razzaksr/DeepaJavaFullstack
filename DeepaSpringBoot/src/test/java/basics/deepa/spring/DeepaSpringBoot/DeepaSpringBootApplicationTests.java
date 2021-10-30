806165b6893012e5118f122716267e2c2b6173ab
razzaksr <razzaksr@gmail.com>
2021-10-30 11:31:11 +0530

    Spring created

---
+++ DeepaSpringBoot/src/test/java/basics/deepa/spring/DeepaSpringBoot/DeepaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package basics.deepa.spring.DeepaSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
